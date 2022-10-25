52e5aed232bbc0423403b4a5206ee27c08469371
AsifKarim7 <asifkarim275@gmail.com>
2022-10-25 15:59:42 +0600

    Removed node_modules folder

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ app.get('/courses', (req, res) => {
     res.send(courses)
 })
 
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find(course => course.id == id);
+    res.send(selectedCourse);
+})
+
 app.listen(port, () => {
     console.log('code adaptive server running on port', port)
 })
